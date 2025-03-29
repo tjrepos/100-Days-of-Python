af81e0b31c8101d7e63d0e858a2a0e3aee4def50
tjrepos <tjnprice@proton.me>
2025-03-29 23:38:06 +1000

    Blah

--- 100DaysofPython/Day5/Password_Generator.py
+++ 100DaysofPython/Day5/Password_Generator.py
@@ -5,30 +5,53 @@ import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-
 password_list = []
-jumbled_list = []
-password_str = ""
-
-for x in range(nr_letters):
-    password_list.append(random.choice(letters))
 
 
-for y in range(nr_symbols):
-    password_list.append(random.choice(symbols))
+def add_letters():
+    nr_letters = int(input("How many letters would you like in your password?\n"))
+    for x in range(nr_letters):
+        password_list.append(random.choice(letters))
+
+def add_symbols():
+    nr_symbols = int(input(f"How many symbols would you like?\n"))
+    for y in range(nr_symbols):
+        password_list.append(random.choice(symbols))
 
 
-for z in range(nr_numbers):
-    password_list.append(random.choice(numbers))
+def add_numbers():
+    nr_numbers = int(input(f"How many numbers would you like?\n"))
+    for z in range(nr_numbers):
+        password_list.append(random.choice(numbers))
 
 
-unjumbled = "".join(password_list)
-password_str = "".join(random.sample(unjumbled, len(unjumbled)))
-print(unjumbled)
-print(f"Your new password is: {password_str}")
+def random_password():
+    password_str = "".join(random.sample(password_list, len(password_list)))
+    print(f"Your new password is: {password_str}")
 
+
+def run():
+    add_letters()
+    add_symbols()
+    add_numbers()
+    random_password()
+    main()
+
+
+def main():
+    print("Welcome to the PyPassword Generator!")
+    menu = input("""Press 1 to generate a password
+    Press q to quit
+    Select an option: """)
+    match menu:
+        case "1":
+            run()
+        case "q":
+            print("PyPassword Generator will now quit...")
+            quit()
+        case _:
+            print("Invalid input. Please try again...")
+            main()
+
+
+main()
